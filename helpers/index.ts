e48274e8d6bd574292b3559c996d78d834a8d75f
KryptomSilver <abelromeror763@gmail.com>
2021-10-01 17:55:13 -0500

    Added helpers for validate

---
+++ helpers/index.ts
@@ -0,0 +1,9 @@
+export const validatePhone = (phone: string) => {
+  const re = /^[+]/g;
+  return re.test(phone);
+};
+export const validateEmail = (email: string) => {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
