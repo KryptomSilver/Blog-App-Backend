c73d1342a3fa7acace5de56bf51d927f207ecd11
KryptomSilver <abelromeror763@gmail.com>
2021-10-03 16:19:58 -0500

    Added sending on auth controller

--- controllers/authCtrl.ts
+++ controllers/authCtrl.ts
@@ -3,8 +3,10 @@ import Users from "../models/userModel";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import { generateActiveToken } from "../config/generateToken";
-import { validateEmail } from "../helpers";
+import { validateEmail, validatePhone } from "../helpers";
 import sendEmail from "../config/sendMail";
+import { sendSMS } from "../config/sendSMS";
+import { IDecodeToken } from "../interfaces/newUser";
 
 const URL_BASE = `${process.env.URL_BASE}`;
 const authCtrl = {
@@ -23,14 +25,37 @@ const authCtrl = {
         account,
       };
       const active_token = generateActiveToken({ newUser });
+      const url = `${URL_BASE}/active/${active_token}`;
       if (validateEmail(account)) {
-        sendEmail(account, URL_BASE, "Verify your email address");
-        res
-          .status(200)
-          .json({ msg: "Success! Please check your email", data: newUser, active_token });
+        sendEmail(account, url, "Verify your email address");
+        res.status(200).json({ msg: "Success! Please check your email" });
+      } else if (validatePhone(account)) {
+        sendSMS(account, url, "Verify your phone number");
+        res.status(200).json({ msg: "Success! Please check your phone" });
       }
     } catch (error) {
       return res.status(500).json({ msg: error });
+    }
+  },
+  activeAccount: async (req: Request, res: Response) => {
+    try {
+      const { active_token } = req.body;
+      const decoded = <IDecodeToken>(
+        jwt.verify(active_token, `${process.env.ACTIVE_TOKEN_SECRET}`)
+      );
+      const { newUser } = decoded;
+      if (!newUser)
+        return res.status(400).json({ msg: "Invalid authentication." });
+      const user = new Users(newUser);
+      await user.save();
+      res.json({ msg: "Account has been activated!" });
+    } catch (error: any) {
+      console.log(error)
+      let errorMsg;
+      if (error.code === 11000) {
+        errorMsg = Object.keys(error.keyValue)[0] + " already exists.";
+      }
+      return res.status(500).json({ msg: errorMsg });
     }
   },
 };
