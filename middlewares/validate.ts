853b4af18fac83e547e8a855dd20361bf3de2080
KryptomSilver <abelromeror763@gmail.com>
2021-10-03 01:39:09 -0500

    Sending the errors in an object

--- middlewares/validate.ts
+++ middlewares/validate.ts
@@ -7,24 +7,22 @@ export const validateRegister = async (
   next: NextFunction
 ) => {
   const { name, account, password } = req.body;
+  const errors = [];
   if (!name) {
-    return res.status(400).json({ msg: "Please add your name." });
+    errors.push("Please add your name.");
   } else if (name.length > 20) {
-    return res.status(400).json({ msg: "Your name is up to 20 chars long." });
+    errors.push("Your name is up to 20 chars long.");
   }
   if (!account) {
-    return res
-      .status(400)
-      .json({ msg: "Please add your account or phone number" });
+    errors.push("Please add your account or phone number");
   } else if (!validateEmail(account) && !validatePhone(account)) {
-    return res
-      .status(400)
-      .json({ msg: "Email or phone number format is incorrect." });
+    errors.push("Email or phone number format is incorrect.");
   }
   if (!password) {
-    return res.status(400).json({ msg: "Please add your password." });
+    errors.push("Please add your password.");
   } else if (password.length < 6) {
-    return res.status(400).json({ msg: "Password must be at least 6 chars" });
+    errors.push("Password must be at least 6 chars");
   }
+  if (errors.length > 0) return res.status(400).json({ msg: errors });
   next();
 };
