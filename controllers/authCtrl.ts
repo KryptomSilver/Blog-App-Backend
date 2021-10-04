305ed2068305a7cfaae358ed80c37e0f6324847d
KryptomSilver <abelromeror763@gmail.com>
2021-10-03 22:27:14 -0500

    Added loging, logout and refresh_token

--- controllers/authCtrl.ts
+++ controllers/authCtrl.ts
@@ -1,12 +1,16 @@
-import { Request, Response } from "express";
+import { Request, Response, Router } from "express";
 import Users from "../models/userModel";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
-import { generateActiveToken } from "../config/generateToken";
+import {
+  generateAccessToken,
+  generateActiveToken,
+  generateRefreshToken,
+} from "../config/generateToken";
 import { validateEmail, validatePhone } from "../helpers";
 import sendEmail from "../config/sendMail";
 import { sendSMS } from "../config/sendSMS";
-import { IDecodeToken } from "../interfaces/newUser";
+import { IDecodeToken, IUser } from "../interfaces/newUser";
 
 const URL_BASE = `${process.env.URL_BASE}`;
 const authCtrl = {
@@ -50,7 +54,7 @@ const authCtrl = {
       await user.save();
       res.json({ msg: "Account has been activated!" });
     } catch (error: any) {
-      console.log(error)
+      console.log(error);
       let errorMsg;
       if (error.code === 11000) {
         errorMsg = Object.keys(error.keyValue)[0] + " already exists.";
@@ -58,6 +62,69 @@ const authCtrl = {
       return res.status(500).json({ msg: errorMsg });
     }
   },
+  login: async (req: Request, res: Response) => {
+    try {
+      const { account, password } = req.body;
+      const user = await Users.findOne({ account });
+      if (!user)
+        return res.status(404).json({ msg: "This account does not exists." });
+      loginUser(user, password, res);
+    } catch (error) {
+      return res.status(500).json({ msg: error });
+    }
+  },
+  logout: async (req: Request, res: Response) => {
+    try {
+      res.clearCookie("refreshtoken", {
+        path: "/api/refresh_token",
+      });
+      return res.json({ msg: "Logged out!" });
+    } catch (error) {
+      return res.status(500).json({ msg: error });
+    }
+  },
+  refreshToken: async (req: Request, res: Response) => {
+    try {
+      const rf_token = req.cookies.refreshtoken;
+      if (!rf_token) return res.status(400).json({ msg: "Please login now!" });
+      const decoded = <IDecodeToken>(
+        jwt.verify(rf_token, `${process.env.REFRESH_TOKEN_SECRET}`)
+      );
+      console.log(typeof decoded.id);
+      if (!decoded.id)
+        return res.status(400).json({ msg: "Please login now!" });
+
+      const user = await Users.findById(decoded.id).select("-password");
+      if (!user)
+        return res.status(400).json({ msg: "This account does not exist." });
+      const access_token = generateAccessToken({ id: user._id });
+      res.json({ access_token });
+    } catch (error) {
+      return res.status(500).json({ msg: "hola" });
+    }
+  },
+};
+const loginUser = async (user: IUser, password: string, res: Response) => {
+  try {
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(500).json({ msg: "Password is incorrect." });
+    const access_token = generateAccessToken({ id: user._id });
+    const refresh_token = generateRefreshToken({ id: user._id });
+    res.cookie("refreshtoken", refresh_token, {
+      httpOnly: true,
+      path: "/api/refresh_token",
+      maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    });
+    res.json({
+      msg: "Login success!",
+      access_token,
+      ...user._doc,
+      password: "",
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export default authCtrl;
