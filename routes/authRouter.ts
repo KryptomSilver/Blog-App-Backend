3503a599a228eb894133b634637470d89534c43f
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-10-15 02:06:45 -0500

    Added endpoints verify sms and login sms.

--- routes/authRouter.ts
+++ routes/authRouter.ts
@@ -10,5 +10,7 @@ router.post("/login", authCtrl.login);
 router.get("/logout", authCtrl.logout);
 router.get("/refresh_token", authCtrl.refreshToken);
 router.post("/google_login", authCtrl.googleLogin);
+router.post("/sms_login", authCtrl.smsLogin);
+router.post("/sms_verify", authCtrl.smsVerify);
 
 export default router;
