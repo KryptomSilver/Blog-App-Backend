2fa33ac32aefc447d7f15b72fee97a6031b5a4c0
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-10-15 02:07:41 -0500

    Functions for check phone number and check code.

--- config/sendSMS.ts
+++ config/sendSMS.ts
@@ -2,6 +2,7 @@ import { Twilio } from "twilio";
 const accountSid = `${process.env.TWILIO_ACCOUNT_SID}`;
 const authToken = `${process.env.TWILIO_AUTH_TOKEN}`;
 const from = `${process.env.TWILIO_PHONE_NUMBER}`;
+const service = `${process.env.TWILIO_SERVICE_SID}`;
 const client = new Twilio(accountSid, authToken);
 
 export const sendSMS = (to: string, body: string, txt: string) => {
@@ -13,6 +14,26 @@ export const sendSMS = (to: string, body: string, txt: string) => {
         to,
       })
       .then((message) => console.log(message.sid));
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const smsOTP = async (to: string, channel: string) => {
+  try {
+    const data = await client.verify
+      .services(service)
+      .verifications.create({ to, channel });
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const smsVerify = async (to: string, code: string) => {
+  try {
+    const data = await client.verify
+      .services(service)
+      .verificationChecks.create({ to, code });
+    return data;
   } catch (error) {
     console.log(error);
   }
