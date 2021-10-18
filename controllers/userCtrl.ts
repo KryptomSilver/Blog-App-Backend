18bebe57b27ed00e6fae0763a01d30f897c92b4e
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-10-18 02:01:46 -0500

    Added reset password function and endpoint.

--- controllers/userCtrl.ts
+++ controllers/userCtrl.ts
@@ -1,7 +1,7 @@
-import { Request, Response } from "express";
+import { Response } from "express";
 import Users from "../models/userModel";
 import { IReqAuth } from "../interfaces";
-
+import bcrypt from "bcrypt";
 const userCtrl = {
   updateUser: async (req: IReqAuth, res: Response) => {
     if (!req.user)
@@ -10,6 +10,28 @@ const userCtrl = {
       const { avatar, name } = req.body;
       await Users.findOneAndUpdate({ _id: req.user._id }, { avatar, name });
       res.json({ msg: "Update Success!" });
+    } catch (error: any) {
+      return res.status(500).json({ msg: error.message });
+    }
+  },
+  resetPassword: async (req: IReqAuth, res: Response) => {
+    if (!req.user)
+      return res.status(400).json({ msg: "Invalid Authentication" });
+    if (req.user.type !== "register")
+      return res
+        .status(400)
+        .json({
+          msg: `Quick login account with ${req.user.type} can't use this function.`,
+        });
+
+    try {
+      const { password } = req.body;
+      const passwordHash = await bcrypt.hash(password, 12);
+      await Users.findOneAndUpdate(
+        { _id: req.user._id },
+        { password: passwordHash }
+      );
+      res.json({ msg: "Reset Password Success!" });
     } catch (error: any) {
       return res.status(500).json({ msg: error.message });
     }
--- routes/userRouter.ts
+++ routes/userRouter.ts
@@ -1,8 +1,10 @@
 import express from "express";
-import auth from '../middlewares/auth'
+import auth from "../middlewares/auth";
 import userCtrl from "../controllers/userCtrl";
 
 const router = express.Router();
 
-router.patch("/user",auth,userCtrl.updateUser)
+router.patch("/user", auth, userCtrl.updateUser);
+router.patch("/reset_password", auth, userCtrl.resetPassword);
+
 export default router;
