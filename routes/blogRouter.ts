2cd821d748965412b8cba274ea67e2a0d8d32389
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-10-29 17:06:43 -0500

    added new end point for blogs

--- routes/blogRouter.ts
+++ routes/blogRouter.ts
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.post("/blog", auth, blogCtrl.createBlog);
 router.get("/blog", blogCtrl.getBlogs);
+router.get("/blog/:id_category", blogCtrl.getBlogsByCategory);
 
 export default router;
