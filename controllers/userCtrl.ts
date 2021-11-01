7050997848335e86e3eb3b7237db8cbc2df0e30c
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-11-01 14:07:34 -0600

    Added get user by id and new endpoint

--- controllers/userCtrl.ts
+++ controllers/userCtrl.ts
@@ -1,4 +1,4 @@
-import { Response } from "express";
+import { Request, Response } from "express";
 import Users from "../models/userModel";
 import { IReqAuth } from "../interfaces";
 import bcrypt from "bcrypt";
@@ -18,11 +18,9 @@ const userCtrl = {
     if (!req.user)
       return res.status(400).json({ msg: "Invalid Authentication" });
     if (req.user.type !== "register")
-      return res
-        .status(400)
-        .json({
-          msg: `Quick login account with ${req.user.type} can't use this function.`,
-        });
+      return res.status(400).json({
+        msg: `Quick login account with ${req.user.type} can't use this function.`,
+      });
 
     try {
       const { password } = req.body;
@@ -32,6 +30,17 @@ const userCtrl = {
         { password: passwordHash }
       );
       res.json({ msg: "Reset Password Success!" });
+    } catch (error: any) {
+      return res.status(500).json({ msg: error.message });
+    }
+  },
+  getUser: async (req: Request, res: Response) => {
+    try {
+      const user = await Users.findOne({ _id: req.params.id }).select(
+        "-password"
+      );
+      if (!user) return res.status(400).json({ msg: "User not found" });
+      res.json(user);
     } catch (error: any) {
       return res.status(500).json({ msg: error.message });
     }
