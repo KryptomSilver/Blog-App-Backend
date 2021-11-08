54afc1cbc74f5dd5a038ec644e8c3565660f1928
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-11-08 11:19:10 -0600

    Added endpoint for comments

--- index.ts
+++ index.ts
@@ -26,6 +26,7 @@ app.use("/api", routes.authRouter);
 app.use("/api", routes.userRouter);
 app.use("/api", routes.categoryRouter);
 app.use("/api", routes.blogRouter);
+app.use("/api", routes.commentRouter);
 
 //Server listening
 const PORT = process.env.PORT || 5000;
